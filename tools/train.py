7097cd2407e7b08d9f6208dac04c348343412260
TnoobT <tianfujing@foxmail.com>
2022-07-21 01:45:06 +0000

    1

--- tools/train.py
+++ tools/train.py
@@ -18,7 +18,7 @@ from mmpose.apis import init_random_seed, train_model
 from mmpose.datasets import build_dataset
 from mmpose.models import build_posenet
 from mmpose.utils import collect_env, get_root_logger, setup_multi_processes
-
+dfgdgdf
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a pose model')
